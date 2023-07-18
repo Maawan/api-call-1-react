c2b8a794d3d796069e1d5a0f37ae919f01e49aeb
Mohd Mawan Ahmad <maawan18@gmail.com>
2023-07-19 05:21:09 +0530

    Work Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
+import Axios from 'axios';
 function App() {
+  const [details , setDetails] = useState({});
+
+  const getDataFromServer = async () => {
+    const {data} = await Axios.get('https://randomuser.me/api');
+    setDetails(data.results);
+  
+  }
+  useEffect(() => {
+    getDataFromServer();
+  } , [])
+  
+  
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card data = {details}/>
   );
 }
 
---
+++ src/Card.js
@@ -0,0 +1,36 @@
+import React from "react";
+function Card({data}){
+    
+    console.log(data)
+    return(
+    
+    <div className="container">
+    <img src={data[0]?.picture.medium} className="logo" alt=""/>
+    <div className="name">
+        <span>{data[0]?.name.title !== undefined ? data[0]?.name.title + ". " + data[0]?.name.first + " " + data[0]?.name.last : ""}</span>
+        {console.log(data.gender)}
+    </div>
+
+    <hr/>
+    <div className="otherInfo">
+        <div className="item">
+            <img src="./email.png"/>
+            <p>{data[0]?.email !== undefined ? data[0]?.email : ""}</p>
+        </div>
+        <div className="item">
+            <img src="./home.png"/>
+            <p>{data[0]?.location.country}</p>
+        </div>
+        <div className="item">
+            <img src="./call.png"/>
+            <p>{data[0]?.phone}</p>
+        </div>
+        <div className="item">
+            <img src="./birthday-cake.png"/>
+            <p>{data[0]?.dob.age}</p>
+        </div>
+    </div>
+</div>
+
+)}
+export default Card;
